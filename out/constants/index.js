73f3370862882257116b60d788f907a96b12e00c
rickschubert <rick@tray.io>
2020-02-15 09:43:02 +0000

    remove old output folder

--- out/constants/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fileToAddTodoNotesTo = "change_tracker.md";
-//# sourceMappingURL=index.js.map
--- out/moveToScriptTodos.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const vscode = require("vscode");
-exports.moveToScriptTodos = () => {
-    vscode.window.showInformationMessage('Hello World!');
-};
-//# sourceMappingURL=moveToScriptTodos.js.map
