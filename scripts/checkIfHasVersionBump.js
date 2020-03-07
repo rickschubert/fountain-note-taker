527120c40d4db48df8760f8c3dbc3c2c6c3e7e82
Rick Schubert <rickschubert@gmx.de>
2020-03-07 18:32:59 +0000

    Add tests to release script

--- scripts/checkIfHasVersionBump.js
+++
@@ -1,9 +0,0 @@
-const shell = require("shelljs")
-
-shell.exec("git fetch origin master && git diff origin/master -- package.json",{silent: true}, (code, stdout, stderr) => {
-    if (stdout.includes("\"versiddon\":")) {
-        console.log("Has version upgrade, perfect")
-    } else {
-        throw new Error("You need to upgrade the version number")
-    }
-})
